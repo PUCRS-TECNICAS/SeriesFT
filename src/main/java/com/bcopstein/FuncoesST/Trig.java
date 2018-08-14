fefab5eb83d28c3fc26027a12de4a88977aedf06
17111233 <17111233@w3873874.portoalegre.pucrsnet.br>
2018-08-14 11:16:21 -0300

    Trig alterada

--- src/main/java/com/bcopstein/FuncoesST/Trig.java
+++ src/main/java/com/bcopstein/FuncoesST/Trig.java
@@ -2,21 +2,39 @@ package com.bcopstein.FuncoesST;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Trig {
+	private double cosX;
+	private double sinX;
+	
+	private List<Double> termos = new LinkedList<Double>();
+	
 	public Iterator<Double> getTermos(){
 		// TODO:
-		throw new UnsupportedOperationException();
+		//throw new UnsupportedOperationException();
+		return termos.iterator();
 	}
 
-	public double cos(double x) {
+	public double cos(double x) { 
 		// TODO:
-		throw new UnsupportedOperationException();
+		//throw new UnsupportedOperationException();
+		termos.clear();
+		int n=0;		
+		while(cosX>Math.pow(10, -6)) {
+			double cosX=0;
+			cosX=((Math.pow(-1,n)/(Util.fatorial(2*n)))*Math.pow(x, 2*n));
+			termos.add(cosX);
+			n++;
+		}
+		return Util.somatorio(0,termos.size(),termos);
 	}
 
 	public double sin(double x) {
 		// TODO:
-		throw new UnsupportedOperationException();
+		//throw new UnsupportedOperationException();
+		return sinX;
 	}
+
 }
