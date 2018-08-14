60f0c71385894ce1b2df3304bb535b59a86f153b
17104166 <17104166@w3873361.portoalegre.pucrsnet.br>
2018-08-14 10:40:57 -0300

    "Classe Lambert Alterada"

--- src/main/java/com/bcopstein/FuncoesST/Lambert.java
+++ src/main/java/com/bcopstein/FuncoesST/Lambert.java
@@ -2,16 +2,27 @@ package com.bcopstein.FuncoesST;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Lambert {
+	LinkedList<Double> terms;
 	public Iterator<Double> getTermos(){
-		// TODO:
-		throw new UnsupportedOperationException();
+		return terms.iterator();
 	}
 	
 	public double W0(double x) {
-		// TODO:
-		throw new UnsupportedOperationException();
+		double lim = 10E-6;// 0.0000006
+		//throw new IllegalArgumentException();
+		if(x< (1/Math.E)) {throw new IllegalArgumentException();}
+		LinkedList<Double> lstResultados = new LinkedList<Double>();
+		for(int n=1;n<lim;n++) {
+			double fracao = Math.pow(-n,n-1);
+			double divisor = Util.fatorial(n);
+			double resultado = (fracao/divisor)*(Math.pow(x,n));
+			lstResultados.add(resultado);
+		}
+		terms = lstResultados;
+		return Util.somatorio(0, lstResultados.size()-1, lstResultados);
 	}
 }
